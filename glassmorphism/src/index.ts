6e2a93f25cea93543e6183546dd70b2b46571f6a
piguavejc <piguavejc@gmail.com>
2023-02-23 00:20:45 -0500

    actualizando proyecto

--- glassmorphism/src/index.ts
+++ glassmorphism/src/index.ts
@@ -1,7 +1,7 @@
-import "./assets/back.png";
-import "./assets/facebook.png";
-import "./assets/profile.png";
-import "./assets/telegram.png";
-import "./assets/twitter.png";
-import "./scss/main.scss";
-import "vanilla-tilt/dist/vanilla-tilt.min.js"
+import './assets/back.png';
+import './assets/facebook.png';
+import './assets/profile.png';
+import './assets/telegram.png';
+import './assets/twitter.png';
+import './scss/main.scss';
+import 'vanilla-tilt/dist/vanilla-tilt.min.js';
